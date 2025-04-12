ab11f8d1346f54f45b590dee58d37bd78e022ce2
Kopec <mark_kopec@dell.com>
2025-04-12 11:42:00 -0500

    adding optimal model

--- GitLabWebhook/CodeReviewServices/OpenAIService.cs
+++ GitLabWebhook/CodeReviewServices/OpenAIService.cs
@@ -42,7 +42,7 @@ namespace CodeReviewServices
             {
                 Endpoint = new Uri(_openAiBaseUrl), // Specify the hostname here
             };
-            _chatClient = new ChatClient("codellama-13b-instruct", apiKeyCredential, options);
+            _chatClient = new ChatClient("llama-3-1-8b-instruct", apiKeyCredential, options);
         }
 
     
